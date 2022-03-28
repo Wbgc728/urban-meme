78ee03b18ee6061f87d28b06766954a35015d661
wbgc728 <wbgc728@gmail.com>
2022-03-28 03:51:09 -0600

    Got view employees add department & roles and add employee functions working

--- server.js
+++ server.js
@@ -85,7 +85,8 @@ function viewDepartments() {
 // WHEN I choose to view all roles
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 function viewRoles() {
-    db.query('SELECT roles.id, roles.title, department.name AS department FROM roles INNER JOIN department ON roles.department_id = department.id', (err, res) => {
+    db.query('SELECT roles.id, roles.title, department.name AS department FROM roles \
+    INNER JOIN department ON roles.department_id = department.id', (err, res) => {
         if (err) throw err;
         console.log('Viewing roles\n');
         console.table(res);
@@ -98,8 +99,16 @@ function viewRoles() {
 // THEN I am presented with a formatted table showing employee data, including employee ids, 
 // first names, last names, job titles, departments, salaries, and managers that the employees report to
 function viewEmployees() {
-    console.log('Viewing all employees\n');
-
+    db.query('SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department\
+     , roles.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee\
+     LEFT JOIN roles ON employee.role_id = roles.id\
+     LEFT JOIN department ON roles.department_id = department.id\
+     LEFT JOIN employee manager ON employee.manager_id = manager.id', (err, res) => {
+        if (err) throw err;
+        console.log('Viewing all employees\n');
+        console.table(res);
+        userInput();
+    })
 }
 
 
@@ -108,23 +117,143 @@ function viewEmployees() {
 function addDepartment() {
     console.log('Adding a department\n');
 
+    inquirer.prompt([{
+            type: 'input',
+            name: 'addDepartment',
+            message: 'What department would you like to add?',
+            validate: addDepartment => {
+                if (addDepartment) {
+                    return true;
+                } else {
+                    console.log('Please enter a department');
+                    return false;
+                }
+            }
+        }])
+        .then(answer => {
+            db.query('INSERT INTO department (name) VALUES (?)', answer.addDepartment, (err, result) => {
+                if (err) throw err;
+                console.log(answer.addDepartment + ' Department added');
+
+                viewDepartments();
+            });
+        });
+
 }
 
 
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 function addRole() {
-    console.log('Adding a role\n');
+    inquirer.prompt([{
+                type: 'input',
+                name: 'addRole',
+                message: 'What role do you want to add?',
+            },
+            {
+                type: 'input',
+                name: 'addSalary',
+                message: 'What is the salary of this role?',
+            }
+        ])
+        .then(answer => {
+            const params = [answer.addRole, answer.addSalary];
 
-}
+            // Need Current list of departments
+            db.query('SELECT name, id FROM department', (err, data) => {
+                if (err) throw err;
+
+                const dept = data.map(({ name, id }) => ({ name: name, value: id }));
+
+                inquirer.prompt([{
+                        type: 'list',
+                        name: 'department',
+                        message: 'What department is this role in?',
+                        choices: dept
+                    }])
+                    .then(departmentChoice => {
+                        const departmentEl = departmentChoice.department;
+                        params.push(departmentEl);
+
+                        db.query('INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)', params, (err, result) => {
+                            if (err) throw err;
+                            console.log('Added ' + answer.role + ' roles');
+
+                            viewRoles();
+                        });
+                    });
+            });
+        });
+};
+
+
 
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, 
 // role, and manager, and that employee is added to the database
 function addEmployee() {
     console.log('Adding an employee');
+    inquirer.prompt([{
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the employee first name?',
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'What is the employee last name?',
+            }
+        ])
+        .then(answer => {
+            const params = [answer.fistName, answer.lastName]
 
-}
+            // Get the current list of roles
+            db.promise().query('SELECT roles.id, role.title FROM role', (err, data) => {
+                if (err) throw err;
+
+                const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+
+                inquirer.prompt([{
+                        type: 'list',
+                        name: 'role',
+                        message: 'What is the employee role?',
+                        choices: roles
+                    }])
+                    .then(roleChoice => {
+                        const role = roleChoice.role;
+                        params.push(role);
+
+                        const managerSql = 'SELECT * FROM employee';
+
+                        connection.promise().query(managerSql, (err, data) => {
+                            if (err) throw err;
+
+                            const managers = data.map(({ id, first_name, last_name }) => ({ name: first_name + " " + last_name, value: id }));
+
+                            // console.log(managers);
+
+                            inquirer.prompt([{
+                                    type: 'list',
+                                    name: 'manager',
+                                    message: 'Who is the employee manager?',
+                                    choices: managers
+                                }])
+                                .then(managerChoice => {
+                                    const manager = managerChoice.manager;
+                                    params.push(manager);
+
+                                    connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', params, (err, result) => {
+                                        if (err) throw err;
+                                        console.log('Employee added')
+
+                                        showEmployees();
+                                    });
+                                });
+                        });
+                    });
+            });
+        });
+};
 
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
